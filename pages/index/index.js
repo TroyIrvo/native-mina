e4a757970f776278e2b4d4ba6deac5d12f02d579
troyirvo <troyirvo@gmail.com>
2019-12-31 18:21:25 +0800

    提交原生小程序代码

---
+++ pages/index/index.js
@@ -0,0 +1,33 @@
+//index.js
+const Api = require('../../api/interface.js')
+//获取应用实例
+//const app = getApp()
+
+Page({
+  data: {
+    dataList:[],
+    CustomBar:this.globalData.CustomBar
+  },
+
+  onLoad: function () {
+    wx.startPullDownRefresh();
+  },
+
+  onPullDownRefresh:function () {
+    let params = {
+      "UserId": "75f4c652-0a6d-4885-bd7f-d2c5998ce551"
+    };
+    Api.getIndexListData("003300900001", params).then(data => {
+      setTimeout(()=>{
+        wx.stopPullDownRefresh()
+        this.setData({dataList:data.body.items})
+      },1000)
+    }).catch(err => {
+       console.log(err);
+       setTimeout(()=>{
+        wx.stopPullDownRefresh()
+      },1000)
+    })
+  }
+
+})
